fd9c261c9e6605c47a3d7d96d7175928debd7b21
Angelina <angik2004@gmail.com>
2026-07-30 11:55:20 +0300

    fix lowercase component filenames

---
+++ frontend/C:\Users\angelina\Downloads\Recipebook\recipebook-frontend/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/19xk_next_dist_compiled_0zqxkli._.js",
+  "static/chunks/19xk_next_dist_shared_lib_1ffeua_._.js",
+  "static/chunks/19xk_next_dist_client_1cv7ixg._.js",
+  "static/chunks/19xk_next_dist_21a2pgl._.js",
+  "static/chunks/1-4i_next_dist_pages__error_139outm.js",
+  "static/chunks/[next]_entry_page-loader_ts_1aoli7m._.js",
+  "static/chunks/19xk_react-dom_0igyy8f._.js",
+  "static/chunks/19xk_1in6fp5._.js",
+  "static/chunks/[root-of-the-server]__02sxxph._.js",
+  "static/chunks/[project]_pages__error_0du2_q-._.js",
+  "static/chunks/turbopack-[project]_pages__error_1z7jl80._.js"
+])
